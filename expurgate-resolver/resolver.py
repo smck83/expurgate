112b8d2b0cc9631de4f73edb84c52502243cb8c5
smck83 <smckellar@gmail.com>
2022-09-18 19:15:19 +1000

    added RUNNING_CONFIG_ON
    by setting RUNNING_CONFIG_ON to 1 a single config will be generated for all domains. This is to support adding and removing domains without needing to restart expurgate-rbldnsd which only learns about config files at run

--- expurgate-resolver/resolver.py
+++ expurgate-resolver/resolver.py
@@ -11,7 +11,7 @@ import requests
 
 
 paddingchar = "^"
-
+#runningconfigon = 1
 
 if 'UPTIMEKUMA_PUSH_URL' in os.environ and re.match('^http.*\/api\/push\/.*\&ping\=',os.environ['UPTIMEKUMA_PUSH_URL'], re.IGNORECASE):
     uptimekumapushurl = os.environ['UPTIMEKUMA_PUSH_URL']
@@ -21,10 +21,17 @@ if 'SOURCE_PREFIX_OFF' in os.environ:
     source_prefix_off = os.environ['SOURCE_PREFIX_OFF']
 else:
     source_prefix_off = False # set to True to be able to run against root domain
+
 if 'SOURCE_PREFIX' in os.environ:
-    source_prefix  = os.environ['SOURCE_PREFIX']
+    source_prefix = os.environ['SOURCE_PREFIX']
 else:
-    source_prefix = ['_xpg8'] #if not specified, store your 'hidden' record at _xpg8.<yourdomain>
+    source_prefix = "_xpg8"
+
+if 'RUNNING_CONFIG_ON' in os.environ:
+    runningconfigon  = int(os.environ['RUNNING_CONFIG_ON'])
+else:
+    runningconfigon  = 0 #if not specified, generate config files separately
+
 if 'MY_DOMAINS' in os.environ:
     domains = os.environ['MY_DOMAINS']
     mydomains = domains.split(' ') # convert input string to list
@@ -34,6 +41,7 @@ else:
     source_prefix_off = True
     mydomains = ['google.com','mimecast.com','microsoft.com','github.com','who.int','apple.com','lenovo.com'] # demo mode
     print("MY_DOMAIN not set, running in demo mode using " + str(mydomains))
+    
 if 'DELAY' in os.environ and int(os.environ['DELAY']) > 29:
     delayBetweenRun = os.environ['DELAY']
 else:
@@ -45,6 +53,13 @@ loop = 0
 # set the depth to count resolutions
 global depth
 depth = 0
+
+def write2disk(src_path,dst_path,myrbldnsdconfig):
+    with open(src_path, 'w') as fp:
+        for item in myrbldnsdconfig:
+            # write each item on a new line
+            fp.write("%s\n" % item)
+    shutil.move(src_path, dst_path)
 
 def uptimeKumaPush (url):
     try:
@@ -179,6 +194,7 @@ def getSPF(domain):
                         otherValues.append(spfPart)
 
 while loop == 0 and mydomains:
+    runningconfig = []
     start_time = time.time()
     print('Generating config for SPF records in ' + str(mydomains))
     domaincount = 0
@@ -224,21 +240,24 @@ while loop == 0 and mydomains:
         header.append("# IP & Subnet: " + str(len(ipmonitor)))
         # Join all the pieces together, ready for file output
         myrbldnsdconfig = header + ip4header + ip4 + ip4block + ip6header + ip6 + ip6block
+        if runningconfigon == 1:
+            runningconfig = runningconfig + myrbldnsdconfig
 
-        # Write the RBLDNSD config file to disk
-        src_path = r'output/'+ domain.replace(".","-")+".staging"
-        dst_path = r'output/'+ domain.replace(".","-")
-        with open(src_path, 'w') as fp:
-            for item in myrbldnsdconfig:
-                # write each item on a new line
-                fp.write("%s\n" % item)
+        else:
+             # Write the RBLDNSD config file to disk
+            src_path = r'output/'+ domain.replace(".","-")+".staging"
+            dst_path = r'output/'+ domain.replace(".","-")
+            write2disk(src_path,dst_path,myrbldnsdconfig)
             print('[' + str(domaincount) +'/'+ str(totaldomaincount) + '] Generating rbldnsd config for SPF records in ' + domain)
             print('[' + str(domaincount) +'/'+ str(totaldomaincount) + '] Your domain ' + domain + ' required ' + str(depth) + ' lookups.')
-        shutil.move(src_path, dst_path)
     if uptimekumapushurl != None:
         end_time = time.time()
         time_lapsed = (end_time - start_time) * 1000 # calculate loop runtime and convert from seconds to milliseconds
         print("Pushing Uptime Kuma - endpoint : " + uptimekumapushurl + str(math.ceil(time_lapsed)))
-        uptimeKumaPush(uptimekumapushurl + str(math.ceil(time_lapsed)))    
+        uptimeKumaPush(uptimekumapushurl + str(math.ceil(time_lapsed)))
+    if runningconfigon == 1:
+        src_path = r'output/running-config.staging'
+        dst_path = r'output/running-config'               
+        write2disk(src_path,dst_path,runningconfig)
     print("Waiting " + str(delayBetweenRun) + " seconds before running again... ")   
     sleep(int(delayBetweenRun)) # wait DELAY in secondsbefore running again.
